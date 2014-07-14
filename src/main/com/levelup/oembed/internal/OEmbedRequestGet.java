0bb5f034ce16e7af676ec84fa215502bae6e2edb
Steve Lhomme <robux4@gmail.com>
2014-07-14 10:07:23 +0200

    use the new AbstractBuilder to make sure we are building a OEmbedRequestGet

--- src/main/com/levelup/oembed/internal/OEmbedRequestGet.java
+++ src/main/com/levelup/oembed/internal/OEmbedRequestGet.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.levelup.http.BaseHttpRequest;
+import com.levelup.http.HttpRequestImpl;
 import com.levelup.http.HttpUriParameters;
 import com.levelup.http.InputStreamParser;
 import com.levelup.http.gson.InputStreamGsonParser;
@@ -14,11 +15,21 @@ import com.levelup.oembed.OEmbedRequest;
 public class OEmbedRequestGet extends BaseHttpRequest<OEmbed> implements OEmbedRequest {
 
 	public OEmbedRequestGet(Context context, String url) {
-		super(new Builder<OEmbed>(context).setUrl(url).setStreamParser(getParser()));
+		this(context, url, null);
 	}
 
 	public OEmbedRequestGet(Context context, String baseUrl, HttpUriParameters uriParams) {
-		super(new Builder<OEmbed>(context).setUrl(baseUrl, uriParams).setStreamParser(getParser()));
+		super(new AbstractBuilder<OEmbed, OEmbedRequestGet>(context) {
+			@Override
+			protected OEmbedRequestGet build(HttpRequestImpl<OEmbed> impl) {
+				return new OEmbedRequestGet(impl);
+			}
+		}.setUrl(baseUrl, uriParams).setStreamParser(getParser())
+				.buildImpl());
+	}
+
+	protected OEmbedRequestGet(HttpRequestImpl<OEmbed> impl) {
+		super(impl);
 	}
 
 	private static InputStreamParser<OEmbed> getParser() {
