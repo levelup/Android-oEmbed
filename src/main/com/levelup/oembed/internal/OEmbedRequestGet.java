cf3863bc74e4b02d1facff9b4159be00d6f55b66
Steve Lhomme <robux4@gmail.com>
2014-08-20 18:35:37 +0200

    use DataTransform to turn any HttpResponse into the data/error type we want (not fully functional yet)

--- src/main/com/levelup/oembed/internal/OEmbedRequestGet.java
+++ src/main/com/levelup/oembed/internal/OEmbedRequestGet.java
@@ -2,17 +2,17 @@ package com.levelup.oembed.internal;
 
 import android.content.Context;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.levelup.http.BaseHttpRequest;
 import com.levelup.http.HttpEngine;
 import com.levelup.http.HttpUriParameters;
-import com.levelup.http.InputStreamParser;
 import com.levelup.http.gson.InputStreamGsonParser;
+import com.levelup.http.parser.ResponseParser;
 import com.levelup.oembed.OEmbed;
 import com.levelup.oembed.OEmbedRequest;
 
 public class OEmbedRequestGet extends BaseHttpRequest<OEmbed> implements OEmbedRequest {
+
+	private static final InputStreamGsonParser<OEmbed> OEMBED_PARSER = new InputStreamGsonParser<OEmbed>(OEmbed.class);
 
 	public OEmbedRequestGet(Context context, String url) {
 		this(context, url, null);
@@ -24,7 +24,7 @@ public class OEmbedRequestGet extends BaseHttpRequest<OEmbed> implements OEmbedR
 			protected OEmbedRequestGet build(HttpEngine<OEmbed> impl) {
 				return new OEmbedRequestGet(impl);
 			}
-		}.setUrl(baseUrl, uriParams).setStreamParser(getParser())
+		}.setUrl(baseUrl, uriParams).setDataParser(getParser())
 				.buildImpl());
 	}
 
@@ -32,8 +32,7 @@ public class OEmbedRequestGet extends BaseHttpRequest<OEmbed> implements OEmbedR
 		super(impl);
 	}
 
-	private static InputStreamParser<OEmbed> getParser() {
-		Gson gson = new GsonBuilder().create();
-		return new InputStreamGsonParser<OEmbed>(gson, OEmbed.class);
+	private static ResponseParser<OEmbed,?> getParser() {
+		return new ResponseParser<OEmbed, Object>(OEMBED_PARSER, null);
 	}
 }
